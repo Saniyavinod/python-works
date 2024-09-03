3a118977d7354cff78635b9332a8745d7ca23a1a
Saniyavinod <sanisani88341@gmail.com>
2024-09-03 11:59:16 +0530

    send

---
+++ OOPS/Oops_works/class_object.py
@@ -0,0 +1,27 @@
+class student:
+    name: str
+    id: int
+    gender: str  # Changed from int to str
+    address: str
+    age: int
+
+    def set_student(self, name, id, gender, address, age):
+        self.name = name
+        self.id = id
+        self.gender = gender
+        self.address = address
+        self.age = age
+
+    def display_student(self):  # Corrected indentation
+        print(self.name, self.id, self.gender, self.address, self.age)
+
+
+# Create object
+student_instance = student()
+
+student_instance.set_student("saniya", 6, "female", "hhh", 89)
+student_instance.display_student()  # Call the display_student method to show student details
+
+
+
+# 
